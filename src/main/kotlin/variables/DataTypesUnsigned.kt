8a29fcb1c4f1834520f90b18330105db8d0bf5b4
luananicolau <luananicolauln@gmail.com>
2025-11-25 11:38:55 -0300

    atualiza

---
+++ src/main/kotlin/variables/DataTypesUnsigned.kt
@@ -0,0 +1,13 @@
+package variables
+
+fun main(){
+
+    /*
+    Byte -> UByte
+    Short -> UShort
+    Int -> UInt
+    Long -> ULong
+
+
+     */
+}
---
+++ src/main/kotlin/variables/VarVal.kt
@@ -0,0 +1,14 @@
+package variables
+
+fun main(){
+
+    //var counter: Int
+    //counter = 10
+
+    //var counter: Int = 10
+
+    var counter = 10
+    counter = 5
+
+    val counter2 = 10
+}
